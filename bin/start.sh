59e7af0dfe0ab43f4ab1c00c23af537e96dfbced
NatsuruShirose <natsuru.shirose@rights-s.jp>
2019-10-18 11:41:49 +0900

    check connection

---
+++ bin/start.sh
@@ -0,0 +1,1 @@
+node -r dotenv/config index.js
--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const net = require('net');
+const logger = require('./lib/logger.js');
+
+const plcHost = process.env.PLC_HOST;
+const plcPort = process.env.PLC_PORT;
+
+logger.debug(`${plcHost}:${plcPort} connecting...`);
+
+var client = new net.Socket();
+
+client.connect(plcPort, plcHost, () => {
+  console.log('connected');
+});
+
+client.on('data', (data) => {
+  console.log(data);
+});
+
+client.on('close', (data) => {
+  console.log('closed');
+});
+
+client.on('error', (error) => {
+  console.error(error);
+});
+
 console.log(1);
