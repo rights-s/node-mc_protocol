2de46e40271ed2feeb260450d7919764bc43b556
uchida <uchida@rights-s.jp>
2019-10-21 11:14:47 +0900

    add open, close

--- index.js
+++ index.js
@@ -1,41 +1,91 @@
+const net = require('net');
 const logger = require('./lib/logger.js');
-const Frame3eClient = require('./lib/frame3e_client.js');
+// const Frame3eClient = require('./lib/frame3e_client.js');
 
-class McProtocol {
+class McProtocolClient {
   constructor(host, port) {
-    logger.debug(`${host}:${port} connecting...`);
+    this._host = host;
+    this._port = port;
+    this._isOpened = false;
+    this.socket = new net.Socket();
 
-    this.client = new Frame3eClient(host, port);
+    logger.debug(3);
+    this.socket.on('data', (data) => {
+      logger.debug(data);
+    });
+
+    this.socket.on('connect', () => {
+      logger.debug('connected');
+      this._isOpened = true;
+    });
+
+    this.socket.on('close', (data) => {
+      logger.debug('closed');
+    });
+
+    this.socket.on('error', (error) => {
+      logger.debug(error);
+    });
   }
 
-  getWord(address, count) {
-    logger.debug(`get words: ${address}`);
-  }
+  async open(host, port) {
+    if (host) this._host = host;
+    if (port) this._port = port;
 
-  setWord(address, values=[0]) {
-    logger.debug(`set words: ${address} - ${values}`);
-  }
-
-  open() {
-    this.client.open();
+    return new Promise(resolve => {
+      this.socket.connect(this._port, this._host, () => {
+        resolve(true);
+      });
+    });
   }
 
   close() {
-    this.client.close();
+    this.socket.destroy();
+    this._isOpened = false;
+  }
+
+  isOpened() {
+    return this._isOpened;
+  }
+
+  async getWord(address, count) {
+    logger.debug(`get words: ${address}`);
+    logger.debug(0xff);
+
+    return new Promise(resolve => {
+      resolve(10);
+    });
+  }
+
+  async setWord(address, values=[0]) {
+    logger.debug(`set words: ${address} - ${values}`);
   }
 }
 
-module.exports = McProtocol;
+module.exports = McProtocolClient;
 
-let a = new McProtocol('192.168.1.210', '3000');
-a.getWord('D2000');
-a.setWord('D2000', [10, 10, 30]);
+//a.getWord('D2000');
+//a.setWord('D2000', [10, 10, 30]);
 
-setTimeout(() => {
+
+let c = async () => {
+  let mcProtocolClient  = new McProtocolClient('192.168.1.210', '3000');
+  await mcProtocolClient.open();
+  let d0 = await mcProtocolClient.getWord('D0', 1);
+  logger.debug(d0);
+  mcProtocolClient.close();
+};
+
+c();
+
+// logger.debug(a.isOpened());
+
+/*
+ setTimeout(() => {
   a.close();
 
   setTimeout(() => {
     a.open();
   }, 1000);
 }, 1000);
-
+*/
